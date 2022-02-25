862f71d04fcb234668381f17b79bbb42c8d3e818
Stitch710 <dima_pichugin_2016@mail.ru>
2022-02-25 21:03:03 +0300

    Добавьте файлы проекта.

---
+++ Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int lim = 1000;
+            for (int i = 0; i < lim; i++)
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
